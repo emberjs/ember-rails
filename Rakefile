49a259919b6f04c268e65a9ae5ab16b77fc99419
Emmanuel Bastien <bastien_emmanuel@yahoo.fr>
2011-06-10 15:27:12 +0200

    Add tests for HandlebarsJS assets processing.

--- Rakefile
+++ Rakefile
@@ -1,2 +1,13 @@
 require "bundler"
 Bundler::GemHelper.install_tasks
+
+require 'rake/testtask'
+
+Rake::TestTask.new(:test) do |t|
+  t.libs << 'lib'
+  t.libs << 'test'
+  t.pattern = 'test/**/*_test.rb'
+  t.verbose = false
+end
+
+task :default => :test
---
+++ test/hjstemplate_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+ 
+  test "page header should include link to asset" do
+    get :index
+    assert_response :success
+    assert_select 'head script[type="text/javascript"][src="/assets/templates/test.js"]', true
+  end
+  
+end
+
+class HjsTemplateTest < ActionController::IntegrationTest
+  
+  test "asset pipeline should serve template" do
+    get "/assets/templates/test.js"
+    assert_response :success
+    assert @response.body == "SC.TEMPLATES[\"templates_test\"] = SC.Handlebars.compile(\"{{test}}\\n\");\n"
+  end
+  
+end
