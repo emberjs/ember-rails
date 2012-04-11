0190b014ff057c7b30dbe60abc6e1fb0c5e19e5c
tchak <paul@chavard.net>
2012-04-11 11:16:08 +0200

    add compat for old requires with deprecation messages

---
+++ vendor/assets/javascripts/ember-dev.js
@@ -0,0 +1,3 @@
+//= require development/ember
+
+ember_deprecate("You should require `ember` instead of `ember-dev`");
---
+++ vendor/assets/javascripts/ember-runtime-dev.js
@@ -0,0 +1,3 @@
+//= require development/ember-runtime
+
+ember_deprecate("You should require `ember-runtime` instead of `ember-runtime-dev`");
