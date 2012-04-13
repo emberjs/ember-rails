3970791d28c7e56e840c6e5c22c22d2844f15215
tomhuda <tomhuda@tilde.io>
2012-04-13 16:30:59 -0700

    Add ArrayController option to controller generator

--- lib/generators/ember/controller_generator.rb
+++ lib/generators/ember/controller_generator.rb
@@ -6,6 +6,7 @@ module Ember
       source_root File.expand_path("../../templates", __FILE__)
     
       desc "Creates a new Ember.js controller"
+      class_option :array, :type => :boolean, :default => false, :desc => "Create an Ember.ArrayController to represent multiple objects"
       
       def create_controller_files
         template 'controller.js', File.join('app/assets/javascripts/ember/controllers', class_path, "#{file_name}_controller.js")
