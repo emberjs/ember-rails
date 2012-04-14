202c360f23b80222c9af717ed2dcbde481acdbda
tomhuda <tomhuda@tilde.io>
2012-04-13 17:52:28 -0700

    Adds a model generator

---
+++ lib/generators/ember/model_generator.rb
@@ -0,0 +1,26 @@
+require 'ember/version'
+
+module Ember
+  module Generators
+    class ModelGenerator < ::Rails::Generators::NamedBase
+      source_root File.expand_path("../../templates", __FILE__)
+      argument :attributes, :type => :array, :default => [], :banner => "field[:type] field[:type] ..."
+      
+      desc "Creates a new Ember.js model"
+      
+      def create_controller_files
+        template 'model.js', File.join('app/assets/javascripts/ember/models', class_path, "#{file_name}.js")
+      end
+      
+      private 
+      
+      def parse_attributes!
+        self.attributes = (attributes || []).map do |attr|
+          attr = attr.split(':')
+          { name: attr[0], type: attr[1] }
+        end
+      end
+      
+    end
+  end
+end
