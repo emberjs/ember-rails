7e1fbc7985560977ab95f600da97fb7236a8048b
tomhuda <tomhuda@tilde.io>
2012-04-13 17:02:03 -0700

    Split controller and array controller templates

--- lib/generators/ember/controller_generator.rb
+++ lib/generators/ember/controller_generator.rb
@@ -9,7 +9,12 @@ module Ember
       class_option :array, :type => :boolean, :default => false, :desc => "Create an Ember.ArrayController to represent multiple objects"
       
       def create_controller_files
-        template 'controller.js', File.join('app/assets/javascripts/ember/controllers', class_path, "#{file_name}_controller.js")
+        file_path = File.join('app/assets/javascripts/ember/controllers', class_path, "#{file_name}_controller.js")
+        if options.array?
+          template 'array_controller.js', file_path
+        else
+          template 'controller.js', file_path
+        end
       end
     end
   end
