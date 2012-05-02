51f1f2bf38962bf6b0d0ed18540742e9828596e5
tomhuda <tomhuda@tilde.io>
2012-05-02 16:49:01 -0700

    Whitespace

--- lib/generators/ember/controller_generator.rb
+++ lib/generators/ember/controller_generator.rb
@@ -4,10 +4,10 @@ module Ember
   module Generators
     class ControllerGenerator < ::Rails::Generators::NamedBase
       source_root File.expand_path("../../templates", __FILE__)
-    
+
       desc "Creates a new Ember.js controller"
       class_option :array, :type => :boolean, :default => false, :desc => "Create an Ember.ArrayController to represent multiple objects"
-      
+
       def create_controller_files
         file_path = File.join('app/assets/javascripts/ember/controllers', class_path, "#{file_name}_controller.js")
         if options.array?
