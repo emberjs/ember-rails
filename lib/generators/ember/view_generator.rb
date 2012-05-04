122014f985ea8585ba65afe1578e8c0bc7ade745
Patrick B. Gibson <patrick@fadeover.org>
2012-05-03 18:22:48 -0700

    Add a view generator.

---
+++ lib/generators/ember/view_generator.rb
@@ -0,0 +1,18 @@
+require 'ember/version'
+
+module Ember
+  module Generators
+    class ViewGenerator < ::Rails::Generators::NamedBase
+      source_root File.expand_path("../../templates", __FILE__)
+      argument :controller_name, :type => :string, :required => true, :desc => "The controller name for this view"
+      
+      desc "Creates a new Ember.js view and associated Handlebars template"
+      
+      def create_model_files
+        template 'view.js', File.join('app/assets/javascripts/ember/views/' + controller_name, class_path, "#{file_name}_view.js")
+        template 'view.handlebars', File.join('app/assets/javascripts/ember/templates/' + controller_name, class_path, "#{file_name}.handlebars")
+      end
+      
+    end
+  end
+end
