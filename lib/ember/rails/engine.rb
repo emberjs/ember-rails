04d002c9b2fa13eb75c83a11ef122ce2d3c6897c
André Arko <aa@squareup.com>
2012-05-09 00:42:19 -0700

    move config setup into a before_configuration block
    also comment engine code

--- lib/ember/rails/engine.rb
+++ lib/ember/rails/engine.rb
@@ -4,15 +4,23 @@ require 'active_model_serializers'
 module Ember
   module Rails
     class Engine < ::Rails::Engine
-      config.handlebars = ActiveSupport::OrderedOptions.new
-      config.handlebars.precompile = ::Rails.env.production?
-      config.handlebars.templates_root = "templates"
-      config.handlebars.templates_path_separator = '/'
+      config.before_configuration do
+        # set up a Handlebars configuration options object
+        config.handlebars = ActiveSupport::OrderedOptions.new
+        # Precompile Handlebars templates in production
+        config.handlebars.precompile = ::Rails.env.production?
+        # Expect templates to be located in templates by default
+        config.handlebars.templates_root = "templates"
+        # Expect template paths to be separated by slashes by default
+        config.handlebars.templates_path_separator = '/'
+      end
 
       initializer "ember_rails.setup", :group => :all do |app|
+        # Enable filters if the relevant libraries are present
         require 'ember/filters/slim' if defined? Slim
         require 'ember/filters/haml' if defined? Haml
 
+        # Register the Handlebars template engine with Sprockets
         app.assets.register_engine '.handlebars', Ember::Handlebars::Template
         app.assets.register_engine '.hbs', Ember::Handlebars::Template
         app.assets.register_engine '.hjs', Ember::Handlebars::Template
