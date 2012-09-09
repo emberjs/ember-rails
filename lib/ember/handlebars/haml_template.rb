3658306254d06f9431fc3d38d27af366978cd9ee
Rozhnov Alexandr <nox73@ya.ru>
2012-09-10 01:02:29 +0400

    add view context to haml templates

--- lib/ember/handlebars/haml_template.rb
+++ lib/ember/handlebars/haml_template.rb
@@ -13,7 +13,19 @@ module Ember
       def prepare; end
 
       def evaluate(scope, locals, &block)
-        Haml::Engine.new(data).render
+        Haml::Engine.new(data).render(nil, {:h => view_context, :helpers => view_context})
+      end
+
+      def view_context
+        Thread.current[:current_view_context] ||= build_view_context
+      end
+
+      def build_view_context
+        ApplicationController.new.view_context.tap do |context|
+          context.controller.request ||= ActionController::TestRequest.new
+          context.request            ||= context.controller.request
+          context.params             ||= {}
+        end
       end
 
     end
