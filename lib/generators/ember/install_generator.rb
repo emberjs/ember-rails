69924cbaafa71c00064ddc75fc3d2ba7d9f43f80
Falk Pauser <falk.pauser@googlemail.com>
2012-10-26 11:05:02 +0200

    Updated install_generator to call 'rake dist'
    Reason: Ember changed its default rake task to 'test'.

--- lib/generators/ember/install_generator.rb
+++ lib/generators/ember/install_generator.rb
@@ -28,10 +28,10 @@ module Ember
           end
 
           Dir.chdir git_root do
-            say_status("building", "bundle && bundle exec rake", :green)
+            say_status("building", "bundle && bundle exec rake dist", :green)
             Bundler.with_clean_env do
               cmd "bundle --gemfile #{gem_file}"
-              cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake}
+              cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake dist}
             end
           end
 
