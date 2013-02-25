60268ccbfd41dbd6892a92a482d92a2a633192de
Adil Jarulin <adil.jarulin@gmail.com>
2013-02-26 00:54:02 +0600

    add resource generator to create view, template, route, and controller

---
+++ lib/generators/ember/resource_generator.rb
@@ -0,0 +1,29 @@
+require 'ember/version'
+require 'generators/ember/generator_helpers'
+
+module Ember
+  module Generators
+    class ResourceGenerator < ::Rails::Generators::NamedBase
+      include Ember::Generators::GeneratorHelpers
+
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Creates a new Ember.js router, controller, view and template"
+
+      class_option :javascript_engine, :desc => "Engine for JavaScripts"
+      class_option :array, :type => :boolean, :default => false, :desc => "Create an Ember.ArrayController to represent multiple objects"
+      class_option :object, :type => :boolean, :default => false, :desc => "Create an Ember.ObjectController to represent a single object"
+
+
+      def create_resource_files
+
+        invoke('ember:route', [ name ], options)
+        invoke('ember:controller', [ name ], options)
+        invoke('ember:view', [ name ], options)
+        invoke('ember:template', [ name ], options) 
+      end
+
+
+    end
+  end
+end
