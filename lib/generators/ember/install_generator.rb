704d3302cd249af0851b8e7bb5a87dad1c2e0619
Carsten Nielsen <heycarsten@gmail.com>
2013-03-01 14:36:51 -0500

    Ensure rake clean is called before rake dist for install head generator.

--- lib/generators/ember/install_generator.rb
+++ lib/generators/ember/install_generator.rb
@@ -29,9 +29,10 @@ module Ember
           end
 
           Dir.chdir git_root do
-            say_status("building", "bundle && bundle exec rake dist", :green)
+            say_status("building", "bundle && bundle exec rake clean && bundle exec rake dist", :green)
             Bundler.with_clean_env do
               cmd "bundle --gemfile #{gem_file}"
+              cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake clean}
               cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake dist}
             end
           end
@@ -65,10 +66,11 @@ module Ember
           end
 
           Dir.chdir git_root do
-            say_status("building", "bundle && bundle exec rake", :green)
+            say_status("building", "bundle && bundle exec rake clean && bundle exec rake dist", :green)
             Bundler.with_clean_env do
               cmd "bundle --gemfile #{gem_file}"
-              cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake}
+              cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake clean}
+              cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake dist}
             end
           end
 
