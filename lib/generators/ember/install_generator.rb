99952a56152f925aed576b18047388b13315d96c
Stefan Penner <stefan.penner@gmail.com>
2013-05-20 20:09:50 -0400

    rails generate ember:install --head now just fetches from the cdn

--- lib/generators/ember/install_generator.rb
+++ lib/generators/ember/install_generator.rb
@@ -1,94 +1,42 @@
 require 'generators/ember/generator_helpers'
+require 'net/http'
+require 'uri'
+require 'fileutils'
 
 module Ember
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
       desc "Install Ember.js into your vendor folder"
-      class_option :head, :type => :boolean, :default => false, :desc => "Download latest Ember.js from GitHub and copy it into your project"
+      class_option :head, :type => :boolean, :default => false, :desc => "Download latest Ember.js from GitHub and fetch it into your project"
 
-      def copy_ember
+      def fetch_ember
         if options.head?
-
-          git_root = File.expand_path "~/.ember"
-          gem_file = File.join git_root, "Gemfile"
-
-          # If it doesn't exist yet
-          unless File.exist?(git_root)
-            command = %{git clone git://github.com/emberjs/ember.js.git "#{git_root}"}
-            say_status("downloading", command, :green)
-
-            cmd command
-          else
-            Dir.chdir git_root do
-              command = "git fetch origin && git reset origin/master --hard"
-              say_status("updating", command, :green)
-
-              cmd command
-            end
-          end
-
-          Dir.chdir git_root do
-            say_status("building", "bundle && bundle exec rake clean dist", :green)
-            Bundler.with_clean_env do
-              cmd "bundle --gemfile #{gem_file}"
-              cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake clean dist}
-            end
-          end
-
-          source_paths << File.join(git_root, "dist")
-
-          copy_file "ember.js", "vendor/assets/ember/development/ember.js"
-          copy_file "ember.min.js", "vendor/assets/ember/production/ember.js"
+          fetch 'http://builds.emberjs.com/ember-latest.js', 'vendor/assets/ember/development/ember.js'
+          fetch 'http://builds.emberjs.com/ember-latest.min.js', 'vendor/assets/ember/production/ember.js'
         end
       end
 
-      def copy_ember_data
+      def fetch_ember_data
         if options.head?
-
-          git_root = File.expand_path "~/.ember-data"
-          gem_file = File.join git_root, "Gemfile"
-
-          # If it doesn't exist yet
-          unless File.exist?(git_root)
-            command = %{git clone git://github.com/emberjs/data.git "#{git_root}"}
-            say_status("downloading", command, :green)
-
-            cmd command
-          else
-            Dir.chdir git_root do
-              command = "git fetch origin && git reset origin/master --hard"
-              say_status("updating", command, :green)
-
-              cmd command
-            end
-          end
-
-          Dir.chdir git_root do
-            say_status("building", "bundle && bundle exec rake clean dist", :green)
-            Bundler.with_clean_env do
-              cmd "bundle --gemfile #{gem_file}"
-              cmd %{BUNDLE_GEMFILE="#{gem_file}" bundle exec rake clean dist}
-            end
-          end
-
-          source_paths << File.join(git_root, "dist")
-
-          copy_file "ember-data.js", "vendor/assets/ember/development/ember-data.js"
-          copy_file "ember-data.min.js", "vendor/assets/ember/production/ember-data.js"
+          fetch 'http://builds.emberjs.com/ember-data-latest.js', 'vendor/assets/ember/development/ember.js'
+          fetch 'http://builds.emberjs.com/ember-data-latest.min.js', 'vendor/assets/ember/production/ember.js'
         end
       end
 
-      private
+    private
 
-      def cmd(command)
-        out = `#{command}`
+      def fetch(from, to)
+        message = "#{from} -> #{to}"
+        say_status("downloading:", message , :green)
 
-        if $?.exitstatus != 0
-          raise "Command error: command `#{command}` in directory #{Dir.pwd} has failed."
+        uri = URI(from)
+
+        FileUtils.mkdir_p File.dirname(to)
+
+        open(to, 'w+') do |output|
+          output.puts Net::HTTP.get(uri).force_encoding("UTF-8")
         end
-        out
       end
-
     end
   end
 end
