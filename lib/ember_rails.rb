emberjs/ember-rails

commit 8600986e635c1b102b66700e0b211ed7a4400d38

remove warning for RAILS_ENV == test; use dev variant for test env.
fixes #184

lib/ember_rails.rb
@@ -21,7 +21,9 @@ module Ember
       end
 
       initializer "ember_rails.setup_vendor", :after => "ember_rails.setup", :group => :all do |app|
-        if variant = app.config.ember.variant
+        if variant = app.config.ember.variant || Rails.env.test?
+          # test environments should default to development
+          variant ||= :development
           # Copy over the desired ember, ember-data, and handlebars bundled in
           # ember-source, ember-data-source, and handlebars-source to a tmp folder. 
           tmp_path = app.root.join("tmp/ember-rails")

test/precompile_test.rb
@@ -39,4 +39,8 @@ class PrecompileTest < TestCase
   def test_precompile_succeeds_in_production_environment
     precompile! 'production'
   end
+
+  def test_precompile_succeeds_in_test_environment
+    precompile! 'test'
+  end
 end

commit 24f5df3958f727a1a31651bb79152822316a1908

Namespace accessing Rails.env
Fixes bug introduced in 8600986e635c1b102b66700e0b211ed7a4400d38

lib/ember_rails.rb
@@ -21,7 +21,7 @@ module Ember
       end
 
       initializer "ember_rails.setup_vendor", :after => "ember_rails.setup", :group => :all do |app|
-        if variant = app.config.ember.variant || Rails.env.test?
+        if variant = app.config.ember.variant || ::Rails.env.test?
           # test environments should default to development
           variant ||= :development
           # Copy over the desired ember, ember-data, and handlebars bundled in