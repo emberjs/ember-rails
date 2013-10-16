b4cf2013e422f023639e289e6dbce0e681142d05
Bert Hajee <hajee@moretIA.com>
2013-10-16 23:53:48 +0200

    Added support for installing from the beta, canary and release channels
    Added support for --ember_only and --ember_data_only
    Added support for the beta, canary and release channel
    Added tests for the ember:install generator
    deprecated the use of --head on ember:install
    changed the README.md to reflect the changes
    Added a full test set for all options

--- lib/generators/ember/install_generator.rb
+++ lib/generators/ember/install_generator.rb
@@ -3,50 +3,119 @@ require 'net/http'
 require 'uri'
 require 'fileutils'
 
+
 module Ember
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
+
+      class InvalidChannel < ::Thor::Error; end
+      class ConflictingOptions < ::Thor::Error; end
+      class Deprecated < ::Thor::Error; end
+
+      ::InvalidChannel = InvalidChannel
+      ::ConflictingOptions = ConflictingOptions
+      ::Deprecated = Deprecated
+
+      class EmberGenerator
+        def ember
+          super()
+        end
+      end
+
       desc "Install Ember.js into your vendor folder"
-      class_option :head, :type => :boolean, :default => false, :desc => "Download latest Ember.js from GitHub and fetch it into your project"
-      class_option :release, :type => :string, :required => false, :desc => "Release type of ember. Choose between 'head', 'beta' or 'canary'"
+      class_option :head, :type => :boolean, :default => false, :desc => "Download Ember.js & Ember data from canary channel. This is deprecated. Use channel instead."
+      class_option :channel, :type => :string, :required => false, :desc => "Ember release channel Choose between 'release', 'beta' or 'canary'"
+      class_option :ember_only, :type => :boolean, :required => false, :desc => "Only download Ember."
+      class_option :ember_data_only, :type => :boolean, :required => false, :desc => "Onky download ember-data"
+
+      def initialize(args = [], options = {}, config = {})
+        super(args, options, config)
+        check_options
+        process_options
+      end
 
 
-      def release_type
-        if options.head? && options.release
-          say_status('conflicting options', '--head prevailed over --release option' , :red)
-        end
-        if options.head?
-          ''
-        else
-          options['release'] + '/'
+      def ember
+        unless options.ember_data_only?
+          get_ember_js_for(:development)
+          get_ember_js_for(:production)
         end
       end
 
-      def fetch_ember
-        fetch "http://builds.emberjs.com/#{release_type}ember-latest.js", 'vendor/assets/ember/development/ember.js'
-        fetch "http://builds.emberjs.com/#{release_type}ember-latest.min.js", 'vendor/assets/ember/production/ember.js'
-      end
-
-      def fetch_ember_data
-        fetch "http://builds.emberjs.com/#{release_type}ember-data-latest.js", 'vendor/assets/ember/development/ember-data.js'
-        fetch "http://builds.emberjs.com/#{release_type}ember-data-latest.min.js", 'vendor/assets/ember/production/ember-data.js'
+      def ember_data
+        begin
+          unless options.ember_only?
+            get_ember_data_for(:development)
+            get_ember_data_for(:production)
+          end
+        rescue Thor::Error
+          say('WARNING: no ember-data files on this channel yet' , :yellow)
+        end
       end
 
     private
+
+      def get_ember_data_for(environment)
+        create_file "vendor/assets/ember/#{environment}/ember-data.js" do
+          fetch "#{base_url}/#{channel}/ember-data.js", "vendor/assets/ember/#{environment}/ember-data.js"
+        end
+      end
+
+      def get_ember_js_for(environment)
+        create_file "vendor/assets/ember/#{environment}/ember.js" do
+          fetch "#{base_url}/#{channel}/ember.js", "vendor/assets/ember/#{environment}/ember.js"
+        end
+      end
+
+      def check_options
+        if options.head? 
+          say('WARNING: --head option is deprecated in favor of --channel=cannary' , :yellow)
+        end
+        if options.head? && options.channel?
+          say 'ERROR: conflicting options. --head and --channel. Use either --head or --channel=<channel>', :red
+          raise ConflictingOptions
+        end
+        if options.channel? && !%w(release beta canary).include?(options[:channel])
+          say 'ERROR: channel can either be release, beta or canary', :red
+          raise InvalidChannel
+        end
+      end
+
+      def process_options
+        if options.head? 
+          @channel = :canary
+        end
+      end
+
+      def base_url
+        'http://builds.emberjs.com'
+      end
+
+      def channel
+        if options.channel
+          @channel ||= options[:channel]
+        else
+          @channel ||= :release
+        end
+      end
+
 
       def fetch(from, to)
         message = "#{from} -> #{to}"
         say_status("downloading:", message , :green)
 
         uri = URI(from)
-
-        FileUtils.mkdir_p File.dirname(to)
-
-        open(to, 'w+') do |output|
-          output.puts "// Fetched from: " + uri.to_s
-          output.puts "// Fetched on: " + Time.now.utc.iso8601.to_s
-          output.puts Net::HTTP.get(uri).force_encoding("UTF-8")
+        output = StringIO.new
+        output.puts "// Fetched from: " + uri.to_s
+        output.puts "// Fetched on: " + Time.now.utc.iso8601.to_s
+        output.puts Net::HTTP.get(uri).force_encoding("UTF-8")
+        output.rewind
+        content = output.read
+        if content.include?('404')
+          say "ERROR: Error reading the content from the channel with url #{from}." , :red
+          raise raise Thor::Error
         end
+        content
       end
     end
   end
