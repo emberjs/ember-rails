101bc69778c4a5e05eaa718a521a6a1f01b67071
Robert Jackson <robert.w.jackson@me.com>
2013-10-31 09:11:35 -0400

    Allow template generator to use a custom ember_path.

--- test/generators/template_generator_test.rb
+++ test/generators/template_generator_test.rb
@@ -8,12 +8,34 @@ class TemplateGeneratorTest < Rails::Generators::TestCase
 
 
   %w(js coffee).each do |engine|
-
     test "template with #{engine} as current engine" do
-      run_generator ["post"]
+      run_generator ["post", "--javascript-engine", engine]
       assert_file "app/assets/javascripts/templates/post.handlebars"
     end
   end
 
+  test "Assert files are properly created with custom path" do
+    custom_path = ember_path("custom")
+    run_generator [ "post", "-d", custom_path ]
+    assert_file "#{custom_path}/templates/post.handlebars"
+  end
+
+  test "Uses config.ember.ember_path" do
+    begin
+      custom_path = ember_path("custom")
+      old, ::Rails.configuration.ember.ember_path = ::Rails.configuration.ember.ember_path, custom_path
+
+      run_generator ["post"]
+      assert_file "#{custom_path}/templates/post.handlebars"
+    ensure
+      ::Rails.configuration.ember.ember_path = old
+    end
+  end
+
+  private
+
+  def ember_path(custom_path = nil)
+   "app/assets/javascripts/#{custom_path}"
+  end
 end
 
