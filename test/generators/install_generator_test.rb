ee6c8f0a8f89f17f924a506ba5519751a884ed5a
Stanley Stuart <dstanley.stuart@gmail.com>
2013-11-27 12:50:08 -0600

    use a beta release of ember-data
    Use a beta release of ember-data until a stable release of ember-data
    is published. Otherwise, users get a 404 with the defaults.

--- test/generators/install_generator_test.rb
+++ test/generators/install_generator_test.rb
@@ -22,7 +22,7 @@ class InstallGeneratorTest < Rails::Generators::TestCase
     VCR.configure do |c|
       c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
       c.hook_into :webmock # or :fakeweb
-      # c.default_cassette_options = { :record => :new_episodes}
+      c.default_cassette_options = { :record => :new_episodes }
     end
   end
 
