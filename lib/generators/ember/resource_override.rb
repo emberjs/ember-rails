083ec4f736e54aeda65778856883d315ee3a3777
Will Strinz <wstrinz@gmail.com>
2014-07-18 14:09:46 -0500

    pass options including javascript-engine when resource generator invokes view generator

--- lib/generators/ember/resource_override.rb
+++ lib/generators/ember/resource_override.rb
@@ -6,6 +6,12 @@ require "generators/ember/view_generator"
 module Rails
   module Generators
     ResourceGenerator.class_eval do
+
+      class_option :javascript_engine, :desc => "Engine for JavaScripts"
+      class_option :ember_path, :type => :string, :aliases => "-d", :default => false, :desc => "Custom ember app path"
+      class_option :with_template, :type => :boolean, :default => false, :desc => "Create template for this view"
+      class_option :app_name, :type => :string, :aliases => "-n", :default => false, :desc => "Custom ember app name"
+
       def add_ember
         say_status :invoke, "ember:model", :white
         with_padding do
@@ -14,7 +20,7 @@ module Rails
 
         say_status :invoke, "ember controller and view (singular)", :white
         with_padding do
-          invoke "ember:view", [singular_name], :object => true
+          invoke "ember:view", [singular_name], options.merge(:object => true)
         end
 
         @_invocations[Ember::Generators::ControllerGenerator].delete "create_controller_files"
@@ -22,7 +28,7 @@ module Rails
 
         say_status :invoke, "ember controller and view (plural)", :white
         with_padding do
-          invoke "ember:view", [plural_name], :array => true
+          invoke "ember:view", [plural_name], options.merge(:array => true)
         end
       end
     end
