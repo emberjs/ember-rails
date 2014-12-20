6aaa15c2bcd01cc314cf3ae6b322775f1c824549
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-12-21 01:57:45 +0900

    Add adapter generator

---
+++ lib/generators/ember/adapter_generator.rb
@@ -0,0 +1,21 @@
+require 'generators/ember/generator_helpers'
+
+module Ember
+  module Generators
+    class AdapterGenerator < ::Rails::Generators::NamedBase
+      include Ember::Generators::GeneratorHelpers
+
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Creates a new Ember.js adapter"
+      class_option :ember_path, :type => :string, :aliases => "-d", :default => false, :desc => "Custom ember app path"
+      class_option :javascript_engine, :desc => "Engine for JavaScripts"
+      class_option :app_name, :type => :string, :aliases => "-n", :default => false, :desc => "Custom ember app name"
+
+      def create_adapter_files
+        file_path = File.join(ember_path, 'adapters', class_path, "#{file_name}_adapter.#{engine_extension}")
+        template "adapter.#{engine_extension}", file_path
+      end
+    end
+  end
+end
---
+++ test/generators/adapter_generator_test.rb
@@ -0,0 +1,71 @@
+require 'test_helper'
+require 'generators/ember/adapter_generator'
+
+class AdapterGeneratorTest < Rails::Generators::TestCase
+  tests Ember::Generators::AdapterGenerator
+  destination File.join(Rails.root, "tmp", "generator_test_output")
+
+  setup :prepare_destination
+
+  def copy_directory(dir)
+    source = Rails.root.join(dir)
+    dest = Rails.root.join("tmp", "generator_test_output", File.dirname(dir))
+
+    FileUtils.mkdir_p dest
+    FileUtils.cp_r source, dest
+  end
+
+  def prepare_destination
+    super
+
+    copy_directory "app/assets/javascripts"
+    copy_directory "config"
+  end
+
+  test "Assert files are properly created" do
+    run_generator %w(ember)
+
+    assert_file "#{ember_path}/adapters/ember_adapter.js"
+  end
+
+  test "Assert files are properly created with custom path" do
+    custom_path = ember_path("custom")
+    run_generator [ "ember", "-d", custom_path ]
+
+    assert_file "#{custom_path}/adapters/ember_adapter.js"
+  end
+
+  test "Assert files are properly created with custom app name" do
+    run_generator [ "ember", "-n", "MyApp" ]
+    assert_file "app/assets/javascripts/adapters/ember_adapter.js", /MyApp\.EmberAdapter/
+  end
+
+  test "Uses config.ember.app_name as the app name" do
+    begin
+      old, ::Rails.configuration.ember.app_name = ::Rails.configuration.ember.app_name, 'MyApp'
+
+      run_generator %w(ember)
+      assert_file "app/assets/javascripts/adapters/ember_adapter.js", /MyApp\.EmberAdapter/
+    ensure
+      ::Rails.configuration.ember.app_name = old
+    end
+  end
+
+  test "Uses config.ember.ember_path" do
+    begin
+      custom_path = ember_path("custom")
+      old, ::Rails.configuration.ember.ember_path = ::Rails.configuration.ember.ember_path, custom_path
+
+      run_generator [ "ember"]
+      assert_file "#{custom_path}/adapters/ember_adapter.js"
+    ensure
+      ::Rails.configuration.ember.ember_path = old
+    end
+  end
+  private
+
+  def ember_path(custom_path = nil)
+   "app/assets/javascripts/#{custom_path}"
+  end
+
+end
