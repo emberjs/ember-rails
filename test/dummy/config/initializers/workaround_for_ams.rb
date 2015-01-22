31d355c3a0a01ad8601127eaf41fdfaf8fd5bdf1
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-01-23 00:35:34 +0900

    Remove workaround for ActiveModelSerializers gem
    This workaround is no longer needed in 0.9.3.

--- test/dummy/config/initializers/workaround_for_ams.rb
+++
@@ -1,3 +0,0 @@
-# Avoid conflict ActiveModel::Serializers 0.9.2 with Rails 4.2.0: https://github.com/rails-api/active_model_serializers/pull/759
-# TODO Remove this code when it solved.
-::ActionController::Serialization.enabled = false
