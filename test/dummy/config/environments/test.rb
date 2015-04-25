d772cd547acb9325bb3c5c359c0d8921ab4451d9
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-04-25 17:24:57 +0900

    Use `config.serve_static_files` for Rails 4.2
    To suppress the following warning:
    ```
    DEPRECATION WARNING: The configuration option
    `config.serve_static_assets` has been renamed to
    `config.serve_static_files` to clarify its role (it merely enables
    serving everything in the `public` folder and is unrelated to the asset
    pipeline). The `serve_static_assets` alias will be removed in Rails 5.0.
    Please migrate your configuration files accordingly.
    ```

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -10,7 +10,11 @@ Dummy::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  if Rails.version >= '4.2.0'
+    config.serve_static_files = true
+  else
+    config.serve_static_assets = true
+  end
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
