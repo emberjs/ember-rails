803fe0faec1aefdeb1d243e789e9a075d4714ba0
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-03-20 19:21:48 +0900

    [Refactor] Use `%r` literal for function call without brackets
    To suppress the following warning:
    ```
    warning: ambiguous first argument; put parentheses or a space even after
    `/' operator
    ```

--- test/hjstemplate_test.rb
+++ test/hjstemplate_test.rb
@@ -15,26 +15,26 @@ class HjsTemplateTest < IntegrationTest
   test "asset pipeline should serve template" do
     get "/assets/templates/test.js"
     assert_response :success
-    assert_match /Ember\.TEMPLATES\["test"\] = Ember\.(?:Handlebars|HTMLBars)\.template\(/m, @response.body
+    assert_match %r{Ember\.TEMPLATES\["test"\] = Ember\.(?:Handlebars|HTMLBars)\.template\(}m, @response.body
   end
 
   test "asset pipeline should serve bundled application.js" do
     get "/assets/application.js"
     assert_response :success
-    assert_match /Ember\.TEMPLATES\["test"\] = Ember\.(?:Handlebars|HTMLBars)\.template\(/m, @response.body
+    assert_match %r{Ember\.TEMPLATES\["test"\] = Ember\.(?:Handlebars|HTMLBars)\.template\(}m, @response.body
   end
 
   test "should unbind mustache templates" do
     get "/assets/templates/hairy.js"
     assert_response :success
-    assert_match /Ember\.TEMPLATES\["hairy(\.mustache)?"\] = Ember\.(?:Handlebars|HTMLBars)\.template\(/m, @response.body
-    assert_match /function .*unbound|"name":"unbound"/m, @response.body
+    assert_match %r{Ember\.TEMPLATES\["hairy(\.mustache)?"\] = Ember\.(?:Handlebars|HTMLBars)\.template\(}m, @response.body
+    assert_match %r{function .*unbound|"name":"unbound"}m, @response.body
   end
 
   test "ensure new lines inside the anon function are persisted" do
     get "/assets/templates/new_lines.js"
     assert_response :success
-    assert @response.body =~ /; data = data || {};\n|"data":data/, @response.body.inspect
+    assert @response.body =~ %r(; data = data || {};\n|"data":data), @response.body.inspect
   end
 
 end
