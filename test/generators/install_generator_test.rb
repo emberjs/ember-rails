428dbb581afa3fb9c3b6dcd916454954c3b885a6
htwroclau <thajime207@gmail.com>
2018-06-02 14:40:01 +0900

    Update JRuby verion on Travis CI

--- test/generators/install_generator_test.rb
+++ test/generators/install_generator_test.rb
@@ -19,7 +19,7 @@ class InstallGeneratorTest < Rails::Generators::TestCase
   def set_test_environment
     VCR.configure do |c|
       c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
-      c.hook_into :faraday # or :webmock or :fakeweb
+      c.hook_into :webmock # or :fakeweb
       c.default_cassette_options = { :record => :new_episodes }
     end
   end
